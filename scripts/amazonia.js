fb8b3f3fbf9f3e59a7059ac1c19069deea81a969
unknown <valentinobadalucco@gmail.com>
2021-02-16 01:32:26 +0100

    Fine JS + modifiche di stile

--- scripts/amazonia.js
+++ scripts/amazonia.js
@@ -1,11 +1,21 @@
 
 $(document).ready(function (){
+  let theme = $("#theme").attr("href");
+  if(theme == "styles/light-theme.css"){
+    $("#mode_switch").prop("src", "res/dark-theme.png");
+  }else{
+    $("#mode_switch").prop("src", "res/light-theme.png");
+  }
+
   $("#mode_switch").on("click", function () {
     console.log("nice");
     let theme = $("#theme").attr("href");
-    if(theme == "styles/light-theme.css")
+    if(theme == "styles/light-theme.css"){
       $("#theme").prop("href", "styles/dark-theme.css");
-    else
+      $("#mode_switch").prop("src", "res/light-theme.png");
+    }else{
       $("#theme").prop("href", "styles/light-theme.css");
+      $("#mode_switch").prop("src", "res/dark-theme.png");
+    }
   });
 });
